628910ec99bdfce3874b97fe916dbf8b67f99d5e
Andrew Berry <deviantintegral@gmail.com>
2011-06-02 11:17:12 -0400

    Issue #1173692: Implement basic content protection for filefields using jPlayer.

--- theme/jplayer.js
+++ theme/jplayer.js
@@ -67,6 +67,18 @@ Drupal.behaviors.jPlayer = function(context) {
     // is used.
     $(wrapper).find('a.jp-play').click(function() {
       Drupal.jPlayer.pauseOthers(wrapper, player);
+
+      // Generate the authorization URL to ping.
+      var time = new Date;
+      var authorize_url = Drupal.settings.basePath + 'jplayer/authorize/' + base64_encode($(player).attr('rel')) + '/' + base64_encode(parseInt(time.getTime() / 1000).toString());
+
+      // Ping the authorization URL. We need to disable async so that this
+      // command finishes before thisandler returns.
+      $.ajax({
+        url: authorize_url,
+        async: false,
+      });
+      return false;
     });
 
     // Actually initialize the player.
@@ -157,4 +169,114 @@ Drupal.jPlayer.previous = function(wrapper, player, playlist, current) {
   return index;
 }
 
+Drupal.jPlayer.base64Encode = function(data) {
+  // From http://phpjs.org/functions/base64_encode:358 where it is
+  // dual licensed under GPL/MIT.
+  //
+  // http://kevin.vanzonneveld.net
+  // +   original by: Tyler Akins (http://rumkin.com)
+  // +   improved by: Bayron Guevara
+  // +   improved by: Thunder.m
+  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +   bugfixed by: Pellentesque Malesuada
+  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // -    depends on: utf8_encode
+  // *     example 1: base64_encode('Kevin van Zonneveld');
+  // *     returns 1: 'S2V2aW4gdmFuIFpvbm5ldmVsZA=='
+  var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+  var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+      ac = 0,
+      enc = "",
+      tmp_arr = [];
+
+  if (!data) {
+      return data;
+  }
+
+  data = utf8_encode(data + '');
+
+  do { // pack three octets into four hexets
+      o1 = data.charCodeAt(i++);
+      o2 = data.charCodeAt(i++);
+      o3 = data.charCodeAt(i++);
+
+      bits = o1 << 16 | o2 << 8 | o3;
+
+      h1 = bits >> 18 & 0x3f;
+      h2 = bits >> 12 & 0x3f;
+      h3 = bits >> 6 & 0x3f;
+      h4 = bits & 0x3f;
+
+      // use hexets to index into b64, and append result to encoded string
+      tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
+  } while (i < data.length);
+
+  enc = tmp_arr.join('');
+
+  switch (data.length % 3) {
+  case 1:
+      enc = enc.slice(0, -2) + '==';
+      break;
+  case 2:
+      enc = enc.slice(0, -1) + '=';
+      break;
+  }
+
+  return enc;
+};
+
+Drupal.jPlayer.utf8_encode = function(argString) {
+  // From http://phpjs.org/functions/utf8_encode:577 where it is dual-licensed
+  // under GPL/MIT.
+  // http://kevin.vanzonneveld.net
+  // +   original by: Webtoolkit.info (http://www.webtoolkit.info/)
+  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +   improved by: sowberry
+  // +    tweaked by: Jack
+  // +   bugfixed by: Onno Marsman
+  // +   improved by: Yves Sucaet
+  // +   bugfixed by: Onno Marsman
+  // +   bugfixed by: Ulrich
+  // +   bugfixed by: Rafal Kukawski
+  // *     example 1: utf8_encode('Kevin van Zonneveld');
+  // *     returns 1: 'Kevin van Zonneveld'
+
+  if (argString === null || typeof argString === "undefined") {
+      return "";
+  }
+
+  var string = (argString + ''); // .replace(/\r\n/g, "\n").replace(/\r/g, "\n");
+  var utftext = "",
+      start, end, stringl = 0;
+
+  start = end = 0;
+  stringl = string.length;
+  for (var n = 0; n < stringl; n++) {
+      var c1 = string.charCodeAt(n);
+      var enc = null;
+
+      if (c1 < 128) {
+          end++;
+      } else if (c1 > 127 && c1 < 2048) {
+          enc = String.fromCharCode((c1 >> 6) | 192) + String.fromCharCode((c1 & 63) | 128);
+      } else {
+          enc = String.fromCharCode((c1 >> 12) | 224) + String.fromCharCode(((c1 >> 6) & 63) | 128) + String.fromCharCode((c1 & 63) | 128);
+      }
+      if (enc !== null) {
+          if (end > start) {
+              utftext += string.slice(start, end);
+          }
+          utftext += enc;
+          start = end = n + 1;
+      }
+  }
+
+  if (end > start) {
+      utftext += string.slice(start, stringl);
+  }
+
+  return utftext;
+};
+
 })(jQuery);
+
