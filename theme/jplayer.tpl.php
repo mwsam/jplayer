7ed353fc92c6ac3a0c35ea32e64b4f913a11dab5
Sam Men Wee <mwsam.mws@gmail.com>
2012-02-25 10:17:06 +0800

    Update theme layer and JS to be similar to 7.x-2.x branch.

--- theme/jplayer.tpl.php
+++ theme/jplayer.tpl.php
@@ -1,52 +1,52 @@
 <?php
-
 /**
- * Provide the HTML output of the jPlayer audio player.
+ * @file
+ * Provide the HTML output for a jPlayer interface.
  */
 ?>
 
 <div class="jp-<?php print $type; if($type == 'video') print ' jp-video-360p'; ?>">
-<div class="jp-<?php print $mode; ?>-player">
-<div id="<?php print $player_id; ?>" class="jplayer"<?php print $mode == 'single' ? ' rel="' . $item['url'] . '"' : ''; ?>></div>
-<div id="<?php print $player_id . '-interface'; ?>" class="jp-interface">
-    <ul class="jp-controls">
-      <li><a href="#" id="<?php print $player_id; ?>-play" class="jp-play" title="<?php print t('play'); ?>"><?php print t('play'); ?></a></li>
-      <li><a href="#" id="<?php print $player_id; ?>-pause" class="jp-pause" title="<?php print t('pause'); ?>"><?php print t('pause'); ?></a></li>
-      <li><a href="#" id="<?php print $player_id; ?>-stop"class="jp-stop" title="<?php print t('stop'); ?>"><?php print t('stop'); ?></a></li>
-
-      <li><a href="#" id="<?php print $player_id; ?>-mute" class="jp-mute" title="<?php print t('mute'); ?>"><?php print t('mute'); ?></a></li>
-      <li><a href="#" id="<?php print $player_id; ?>-unmute" class="jp-unmute" title="<?php print t('unmute'); ?>"><?php print t('unmute'); ?></a></li>
-      <li><a href="#" id="<?php print $player_id; ?>-volume-max" class="jp-volume-max" title="<?php print t('max volume'); ?>"><?php print t('max volume'); ?></a></li>
-      <?php if ($mode == 'playlist'): ?>
-
-      <li><a href="#" id="<?php print $player_id; ?>-volume-previous" class="jp-previous" title="<?php print t('previous'); ?>"><?php print t('previous'); ?></a></li>
-      <li><a href="#" id="<?php print $player_id; ?>-volume-next" class="jp-next" title="<?php print t('next'); ?>"><?php print t('next'); ?></a></li>
+  <div class="jp-type-playlist">
+    <div id="<?php print $player_id; ?>" class="jp-jplayer"></div>
+    <div id="<?php print $player_id; ?>_interface" class="jp-interface">
+      <?php if ($type == 'video'): ?>
+      <div class="jp-video-play"></div>
       <?php endif; ?>
-
-    </ul>
-    <div id="<?php print $player_id; ?>-progress" class="jp-progress" title="<?php print t('progress'); ?>">
-      <div id="<?php print $player_id; ?>-seek-bar" class="jp-seek-bar">
-        <div id="<?php print $player_id; ?>-play-bar" class="jp-play-bar"></div>
+      <ul class="jp-controls">
+        <li><a href="#" class="jp-play" tabindex="1">play</a></li>
+        <li><a href="#" class="jp-pause" tabindex="1">pause</a></li>
+        <li><a href="#" class="jp-stop" tabindex="1">stop</a></li>
+        <li><a href="#" class="jp-mute" tabindex="1">mute</a></li>
+        <li><a href="#" class="jp-unmute" tabindex="1">unmute</a></li>
+        <?php if ($mode == 'playlist'): ?>
+        <li><a href="#" class="jp-previous" tabindex="1">previous</a></li>
+        <li><a href="#" class="jp-next" tabindex="1">next</a></li>
+        <?php endif; ?>
+      </ul>
+      
+      <div class="jp-progress">
+        <div class="jp-seek-bar">
+          <div class="jp-play-bar"></div>
+        </div>
       </div>
+      
+      <div class="jp-volume-bar">
+        <div class="jp-volume-bar-value"></div>
+      </div>
+      
+      <div class="jp-current-time"></div>
+      <div class="jp-duration"></div>
     </div>
-
-    <div id="<?php print $player_id; ?>-volume-bar" class="jp-volume-bar" title="<?php print t('volume'); ?>">
-      <div id="<?php print $player_id; ?>-volume-bar-value" class="jp-volume-bar-value"></div>
-    </div>
-    <div id="<?php print $player_id; ?>-current-time" class="jp-current-time"></div>
-    <div id="<?php print $player_id; ?>-duration" class="jp-duration"></div>
-  </div>
-
-  <div id="<?php print $player_id; ?>-playlist" class="jp-playlist">
-    <ul>
-      <?php if ($mode == 'playlist'): ?>
-        <?php foreach ($items as $number => $item): ?>
-          <li<?php print $item['class'] ? ' class="' . $item['class'] . '"' : '' ?>><a href="<?php print $item['url']; ?>" title="<?php print check_plain($item['label']); ?>"><?php print check_plain($item['label']); ?></a></li>
-        <?php endforeach; ?>
+    
+    <div id="<?php print $player_id; ?>_playlist" class="jp-playlist">
+      <?php if ($mode == 'playlist' || $mode == 'single'): ?>
+        <?php print $playlist; ?>
       <?php else: ?>
-        <li><?php print check_plain($item['label']); ?></li>
+        <ul>
+          <li><?php print check_plain($label); ?></li>
+        </ul>
       <?php endif; ?>
-    </ul>
+    </div>
   </div>
 </div>
-</div>
+
